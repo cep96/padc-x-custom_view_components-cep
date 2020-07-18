6b754b2b8956c57b428140a44d2fabb76c2b0c9f
Chaw Ei Phyu <hello.chaweiphyu@gmail.com>
2020-07-18 22:39:28 +0630

    data binding logic and handle user action

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/viewpods/ReactionViewPod.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/viewpods/ReactionViewPod.kt
@@ -3,15 +3,27 @@ package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.veiws.viewpods
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.RelativeLayout
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.data.vos.NewsVO
 import kotlinx.android.synthetic.main.view_pod_reaction.view.*
 
 class ReactionViewPods @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
 
+    private var mDelegate: Delegate ?= null
+
     override fun onFinishInflate() {
         super.onFinishInflate()
         setUpListener()
+    }
+
+    fun setDelegate(delegate: Delegate){
+        mDelegate = delegate
+    }
+
+    fun bindData(news: NewsVO){
+        tvLikedUser.text = news.likedUsers.count().toString()
+        tvCommentedUser.text = news.likedUsers.count().toString()
     }
 
     private fun setUpListener() {
@@ -26,5 +38,11 @@ class ReactionViewPods @JvmOverloads constructor(
         tvShare.setOnClickListener {
             
         }
+    }
+
+    interface Delegate{
+        fun onTapLike()
+        fun onTapComment()
+        fun onTapShare()
     }
 }
