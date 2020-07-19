2e2a308e304fa83ce71f53f35ab3c5b4f421f732
Chaw Ei Phyu <hello.chaweiphyu@gmail.com>
2020-07-19 22:49:47 +0630

    add custom attributes to RoundedImageView

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/RoundedCornerImageView.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/RoundedCornerImageView.kt
@@ -6,14 +6,22 @@ import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.withStyledAttributes
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
 
 class RoundedCornerImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    private val cornerRadius = 32f
+    private var cornerRadius = 0f
 
     private val path = Path()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.RoundedCornerImageView){
+            cornerRadius = getDimension(R.styleable.RoundedCornerImageView_cornerRadius, 0f)
+        }
+    }
 
     override fun onDraw(canvas: Canvas?) {
         val rectangle = RectF(0f, 0f, width.toFloat(), height.toFloat())
